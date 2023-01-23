6f11e78a62aeb4f8375894f9254ed9c60f406835
Vladislav <53581923+Shiker2032@users.noreply.github.com>
2023-01-23 11:01:30 +0300

    Пример работы с двумерным массивом

--- WorkingWith2DArray.cpp
+++ WorkingWith2DArray.cpp
@@ -1,5 +1,27 @@
 #include <iostream>
 
-int main() {
+int main()
+{
+	int arr[100][100]{};
+	int n = 0, m = 0;
 
+	printf("n = ");
+	scanf_s("%i", &n);
+
+	printf("m = ");
+	scanf_s("%i", &m);
+
+	for (int i = 0; i < n; i++) {		
+		for (int j = 0; j < m; j++) {
+			scanf_s("%i", &arr[i][j]);
+		}
+		printf("\n");
+	}
+
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < m; j++) {
+			printf("%i ", arr[i][j]);
+		}
+		printf("\n");
+	}
 }
